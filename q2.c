6e61592a5327bccd33702231b4716e36e272738c
Robert Amenta <robert.amenta@uoit.net>
2018-04-02 14:46:24 -0400

    I'm not sure if this is right

--- q2.c
+++ q2.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include <mpi.h>
 
-#define PING_PONG_LIMIT 10
+#define PING_PONG_LIMIT 100
+#define ARRAY (sizeof(double) * 10000000)
+
 int main (int argc, char* argv[])
 {
+	double *p = malloc(ARRAY);
 	MPI_Init(&argc, &argv);
 	int world_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
@@ -16,17 +19,19 @@ int main (int argc, char* argv[])
 	    if (world_rank == ping_pong_count % 2) {
 		// Increment the ping pong count before you send it
 		ping_pong_count++;
-		MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0,
+		MPI_Send(&p, 1, MPI_INT, partner_rank, 0,
 		         MPI_COMM_WORLD);
 		printf("%d sent and incremented ping_pong_count "
 		       "%d to %d\n", world_rank, ping_pong_count,
 		       partner_rank);
 	    } else {
-		MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0,
+		ping_pong_count++;
+		MPI_Recv(&p, 1, MPI_INT, partner_rank, 0,
 		         MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		printf("%d received ping_pong_count %d from %d\n",
 		       world_rank, ping_pong_count, partner_rank);
 	    }
 	}
+	printf("%f\n", MPI_Wtime());
 	MPI_Finalize();
 }
